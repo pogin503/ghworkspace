6a8ac180fabc7416292183abe52f99f236f8e8ca
pogin503 <pogin503@gmail.com>
2017-03-23 00:10:03 +0900

    Add Java examples

---
+++ language/java/study/practice/practice/Usage.java
@@ -0,0 +1,42 @@
+package practice;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Usage {
+
+	public static void main(String[] args) {
+		
+		List<String> list = new ArrayList<>(Arrays.asList("AA", "BB", "CC"));
+		// Print [AA, BB, CC]
+		System.out.println(list);
+		for (String x : list) {
+			// Print
+			// AA
+			// BB
+			// CC
+			System.out.println(x);
+		}
+		
+		List<String> list1 = new ArrayList<>();
+		for (String item : new String[]{"AA", "BB", "CC"}) {
+		    list1.add(item);
+		}
+		
+		List<String> list2 = new ArrayList<String>(){
+		    {
+		        add("AA");
+		        add("BB");
+		        add("CC");
+		    }
+		};
+		
+		List<String> list3 = new ArrayList<String>();
+		Collections.addAll(list2, new String[]{"AA", "BB", "CC"});
+		
+		List<String> list4 = new ArrayList<String>();
+		Collections.addAll(list4, "AA", "BB", "CC");
+	}
+
+}
---
+++ language/java/study/practice/practice/UsageJava8.java
@@ -0,0 +1,35 @@
+package practice;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class UsageJava8 {
+
+	public static void main(String[] args) {
+	    final int[] valArray1 = IntStream.range(0, 10).toArray();
+	    System.out.println(IntStream.of(valArray1).sum());
+	    System.out.println(IntStream.of(1, 2, 3, 4)
+        	.filter(e -> e > 2)
+        	.peek(e -> System.out.println("Filtered value: " + e))
+        	.map(e -> e * e)
+        	.peek(e -> System.out.println("Mapped value: " + e))
+        	.sum());
+	    
+	    final int[] valArray2 = IntStream.rangeClosed(0, 10).toArray();
+	    final long[] valArray3 = LongStream.range(0, 10).toArray();
+	    final long[] valArray4 = LongStream.rangeClosed(0, 10).toArray();
+	    IntStream.rangeClosed(1, 9).forEach( i -> {
+	    	IntStream.rangeClosed(1,9).forEach( j -> {
+	    		System.out.printf("%2d ", i*j);
+	    	});
+            System.out.println();
+	    });
+	    IntStream is = IntStream.range(1,10);
+	    is.forEach( i -> {
+	    	is.forEach( j -> {
+	    		System.out.printf("%2d ", i*j);
+	    	});
+	    	System.out.println();
+	    });
+	}
+
+}
