d65c2404b70bf1a503f6fd6f3e6325c8b20b3269
pogin503 <pogin503@gmail.com>
2021-03-13 03:10:04 +0900

    feat: add python template

---
+++ atcoder/python/template/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+# import sys
+
+def solve1():
+    n = map(int, input().split())
+    # s = [ input() for i in range(N)]
+
+
+def main():
+    solve1()
+
+
+if __name__ == '__main__':
+    main()
