80918edd7925d5b003dc385e6dd02f18063c6e72
pogin503 <pogin503@gmail.com>
2022-12-18 02:25:18 +0900

    feat: commit code up to chapter 5

--- language/c-cpp/lisp/yasalisp/yasalisp.c
+++ language/c-cpp/lisp/yasalisp/yasalisp.c
@@ -1,105 +1,44 @@
-#include "stdio.h"
-#define printf_d(x) printf("%s = %d",#x,(x))
-#define printf_str(x) printf("%s = %s",#x,(x))
-
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP(i,n)  FOR(i,0,n)
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <setjmp.h>
+#include "yasalisp.h"
 
-#define HEAPSIZE 100
-#define FREESIZE 50
-#define STACKSIZE 30000
-#define SYMSIZE 256
-#define BUFSIZE 100
-#define NIL 0
-#define T 4
-
-//-------read--------
-#define EOL     '\n'
-#define TAB     '\t'
-#define SPACE   ' '
-#define ESCAPE  033
-#define NUL     '\0'
-
-typedef enum tag {
-	EMP, NUM, SYM, LIS, SUBR, FSUBR, FUNC
-} tag;
-
-typedef enum flag {
-	FRE, USE
-} flag;
-
-struct cell {
-	tag tag;
-	flag flag;
-	char *name;
-	union {
-		int num;
-		int bind;
-		int(*subr)();
-	} val;
-	int car;
-	int cdr;
-};
-
-typedef struct cell cell;
-
-typedef enum toktype {
-	LPAREN, RPAREN, QUOTE, DOT, NUMBER, SYMBOL, OTHER
-} toktype;
-
-typedef enum backtrack{
-	GO, BACK
-} backtrack;
-
-struct token {
-	char ch;
-	backtrack flag;
-	toktype type;
-	char buf[BUFSIZE];
-} token;
-
-struct token stok = {GO, OTHER};
-
-/*
-implicit conversion from enumeration type 'enum toktype' to different enumeration type 'backtrack' (aka 'enum backtrack')
- */
-#define GET_CAR(addr)       heap[addr].car
-#define GET_CDR(addr)       heap[addr].cdr
-#define GET_NUMBDRE(addr)   heap[addr].val.num
-#define GET_NAME(addr)      heap[addr].name
 //
 cell heap[HEAPSIZE];
+int stack[STACKSIZE];
+int argstk[STACKSIZE];
+struct token stok = {GO,OTHER};
+jmp_buf buf;
 
-// pointer
-int ep; // environment pointer
-int hp; // heap pointer
-int sp; // stack pointer
-int fc; // free counter
-int ap; // arglist pointer
-
-// arg check code
-#define NUMLIST_TEST 1
-#define SYMBOL_TEST  2
-#define NUMBER_TEST  3
-#define LIST_TEST    4
-#define LEN0_TEST    5
-#define LEN1_TEST    6
-#define LEN2_TEST    7
-#define LEN3_TEST    8
-#define LENS1_TEST   9
-#define LENS2_TEST   10
-#define COND_TEST    11
+int main(){
+    printf("MonoLis Ver0.01\n");
+    initcell();
+    initsubr();
+    int ret = setjmp(buf);
+ repl:
+    if(ret == 0) {
+        while(1) {
+            printf("> ");
+            fflush(stdout);
+            fflush(stdin);
+            print(eval(read()));
+            printf("\n");
+            fflush(stdout);
+        }
+    } else {
+        if (ret == 1) {
+            ret = 0;
+            goto repl;
+        } else {
+            return 0;
+        }
+    }
+}
 
 void initcell() {
-	int addr, addr1;
-
-	for(addr = 0; addr < HEAPSIZE; addr++) {
+	for(int addr = 0; addr < HEAPSIZE; addr++) {
 		heap[addr].flag = FRE;
 		heap[addr].cdr = addr + 1;
 	}
@@ -107,20 +46,154 @@ void initcell() {
 	fc = HEAPSIZE;
 
 	ep = makesym("nil");
-	assocym(makesym("nil", NIL));
-	assocym(makesym("t"), makesym("t"));
+	assocsym(makesym("nil"), NIL);
+	assocsym(makesym("t"), makesym("t"));
 	sp = 0;
 	ap = 0;
 }
 
-int symboltoken(char buf[]){
+//変数の束縛
+//ローカル変数の場合は以前の束縛に積み上げていく。
+//したがって同じ変数名があったとしても書き変えない。
+void assocsym(int sym, int val){
+    ep = cons(cons(sym,val),ep);
+}
+
+int issymch(char c){
+    switch(c){
+    case '!':
+    case '?':
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '=':
+    case '<':
+    case '>': return(1);
+    default:  return(0);
+    }
+}
+
+int freshcell() {
+    int res = hp;
+    hp = heap[hp].cdr;
+    SET_CDR(res, 0);
+    fc--;
+    return res;
+}
+
+int makenum(int num) {
+    int addr;
+
+    addr = freshcell();
+    SET_TAG(addr, num);
+    SET_NUMBER(addr, num);
+    return addr;
+}
+
+int makesym(char *name) {
+    int addr;
+
+    addr = freshcell();
+    SET_TAG(addr, SYM);
+    SET_NAME(addr, name);
+    return addr;
+
+}
+
+int car(int addr){
+    return GET_CAR(addr);
+}
+
+int caar(int addr){
+    return car(car(addr));
+}
+
+int cdar(int addr){
+    return cdr(car(addr));
+}
+
+int cdr(int addr){
+    return GET_CDR(addr);
+}
+
+int cadr(int addr){
+    return car(cdr(addr));
+}
+
+int caddr(int addr){
+    return car(cdr(cdr(addr)));
+}
+
+int cons(int car, int cdr) {
+    int addr = freshcell();
+    SET_TAG(addr, LIS);
+    SET_CAR(addr, car);
+    SET_CDR(addr, cdr);
+    return addr;
+}
+
+int assoc(int sym, int lis){
+    if (nullp(lis)) {
+        return(0);
+    }
+    else if(eqp(sym, caar(lis))) {
+        return(car(lis));
+    }
+    else {
+        return(assoc(sym,cdr(lis)));
+    }
+}
+
+int length(int addr){
+    int len = 0;
+
+    while(!(nullp(addr))){
+        len++;
+        addr = cdr(addr);
+    }
+    return(len);
+}
+
+int list(int arglist){
+    if(nullp(arglist))
+        return(NIL);
+    else
+        return(cons(car(arglist),list(cdr(arglist))));
+}
+
+int numbertoken(char buf[]){
     int i;
     char c;
+
+    if(((buf[0] == '+') || (buf[0] == '-'))){
+        if(buf[1] == NUL)
+            return(0); // case {+,-} => symbol
+        i = 1;
+        while((c=buf[i]) != NUL)
+            if(isdigit(c))
+                i++;  // case {+123..., -123...}
+            else
+                return(0);
+    }
+    else {
+        i = 0;    // {1234...}
+        while((c=buf[i]) != NUL)
+            if(isdigit(c))
+                i++;
+            else
+                return(0);
+    }
+    return(1);
+}
+
+int symboltoken(char buf[]){
 
     if(isdigit(buf[0]))
         return(0);
 
-    i = 0;
+    int i = 0;
+    char c;
     while((c=buf[i]) != NUL)
         if((isalpha(c)) || (isdigit(c)) || (issymch(c)))
             i++;
@@ -130,13 +203,9 @@ int symboltoken(char buf[]){
     return(1);
 }
 
-/* void gettoken(); */
-
 //-------read()--------
 
 void gettoken(void){
-    char c;
-    int pos;
 
     if(stok.flag == BACK){
         stok.flag = GO;
@@ -155,10 +224,11 @@ void gettoken(void){
         return;
     }
 
-    c = getchar();
+    char c = getchar();
     while((c == SPACE) || (c == EOL) || (c == TAB))
         c=getchar();
 
+    int pos;
     switch(c){
         case '(':   stok.type = LPAREN; break;
         case ')':   stok.type = RPAREN; break;
@@ -185,7 +255,6 @@ void gettoken(void){
     }
 }
 
-
 /* void gettoken(){ */
 /* 	char c; */
 /* 	int pos; */
@@ -194,7 +263,237 @@ void gettoken(void){
 /* 	} */
 /* } */
 
-int main(){
+int read() {
+    gettoken();
+    switch(stok.type) {
+    case NUMBER: return makenum(atoi(stok.buf));
+    case SYMBOL: return makesym(stok.buf);
+    case QUOTE: return cons(makesym("quote"), cons(read(), NIL));
+    default: error(CANT_READ_ERR, "read", NIL);
+    }
+}
 
-    return 0;
+int readlist() {
+    gettoken();
+
+    if (stok.type == RPAREN) {
+        return NIL;
+    } else if (stok.type == DOT) {
+        int cdr = read();
+        if (atomp(cdr)) {
+            gettoken();
+        }
+        return cdr;
+    } else {
+        stok.flag = BACK;
+        int car = read();
+        int cdr = readlist();
+        return cons(car, cdr);
+    }
+}
+
+void print(int addr) {
+    switch(GET_TAG(addr)) {
+    case NUM: printf("%d", GET_NUMBER(addr)); break;
+    case SYM: printf("%s", GET_NAME(addr)); break;
+    case SUBR: printf("<subr>"); break;
+    case FSUBR: printf("<fsubr>"); break;
+    case FUNC: printf("function"); break;
+    case LIS: {
+        printf("(");
+        printlist(addr);
+        break;
+    }
+    }
+}
+
+void printlist(int addr) {
+    if (IS_NIL(addr)) {
+        printf(")");
+    } else if ((!(listp(cdr(addr)))) && (! (nullp(cdr(addr))))) {
+        print(car(addr));
+        printf(" . ");
+        print(cdr(addr));
+        printf(")");
+    } else {
+        print(GET_CAR(addr));
+        if (! (IS_NIL(GET_CDR(addr)))) {
+            printf(" ");
+        }
+        printlist(GET_CDR(addr));
+    }
+}
+
+int eval(int addr) {
+    if (atomp(addr)) {
+        if (numberp(addr)) {
+            return addr;
+        }
+
+        if (symbolp(addr)) {
+            int res = findsym(addr);
+            if (res == 0) {
+                error(CANT_FIND_ERR, "eval", addr);
+            } else {
+                return res;
+            }
+        }
+    } else if (listp(addr)) {
+        if (listp(addr)) {
+            if (symbolp(car(addr)) && HAS_NAME(car(addr), "quote")) {
+                return cadr(addr);
+            }
+            if (numberp(car(addr))) {
+                error(ARG_SYM_ERR, "eval", addr);
+            }
+            if (subrp(car(addr))) {
+                return apply(car(addr), evlis(cdr(addr)));
+            }
+            if (fsubrp(car(addr))) {
+                return apply(car(addr), cdr(cdr(addr)));
+            }
+            if (functionp(car(addr))) {
+                return apply(car(addr), cdr(cdr(addr)));
+            }
+
+        }
+    }
+    error(CANT_FIND_ERR, "eval", addr);
+}
+
+int atomp(int addr) {
+    if (IS_NUMBER(addr) || IS_SYMBOL(addr)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int numberp(int addr){
+    if(IS_NUMBER(addr))
+        return(1);
+    else
+        return(0);
+}
+
+int symbolp(int addr){
+    if(IS_SYMBOL(addr))
+        return(1);
+    else
+        return(0);
+}
+
+int listp(int addr){
+    if(IS_LIST(addr))
+        return(1);
+    else
+        return(0);
+}
+
+int nullp(int addr){
+    if(IS_NIL(addr))
+        return(1);
+    else
+        return(0);
+}
+
+int eqp(int addr1, int addr2) {
+    if ((numberp(addr1) && numberp(addr2))
+        && (GET_NUMBER(addr1) == GET_NUMBER(addr2))) {
+        return 1;
+    } else if ((symbolp(addr1) && symbolp(addr2))
+               && (SAME_NAME(addr1, addr2))) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int subrp(int addr){
+    int val;
+
+    val = findsym(addr);
+    if(val != -1)
+        return(IS_SUBR(val));
+    else
+        return(0);
+}
+
+int fsubrp(int addr){
+    int val;
+
+    val = findsym(addr);
+    if(val != -1)
+        return(IS_FSUBR(val));
+    else
+        return(0);
+}
+
+int functionp(int addr){
+    int val;
+
+    val = findsym(addr);
+    if(val != -1)
+        return(IS_FUNC(val));
+    else
+        return(0);
+}
+
+//環境は次のように連想リストになっている。
+// env = ((sym1 . val1) (sym2 . val2) ...(t . t)(nil . 0))
+// assocでシンボルに対応する値を探す。
+//見つからなかった場合には-1を返す。
+int findsym(int sym){
+	int addr;
+
+    addr = assoc(sym,ep);
+
+    if(addr == 0)
+    	return(-1);
+    else
+    	return(cdr(addr));
+}
+
+//-------エラー処理------
+void error(int errnum, char *fun, int arg){
+    switch(errnum){
+        case CANT_FIND_ERR:{printf("%s can't find difinition of ", fun);
+                            print(arg); break; }
+
+        case CANT_READ_ERR:{printf("%s can't read of ", fun);
+                            break; }
+
+        case ILLEGAL_OBJ_ERR:{printf("%s got an illegal object ", fun);
+                            print(arg); break; }
+
+        case ARG_SYM_ERR:  {printf("%s require symbol but got ", fun);
+                            print(arg); break; }
+
+        case ARG_NUM_ERR:  {printf("%s require number but got ", fun);
+                            print(arg); break; }
+
+        case ARG_LIS_ERR:  {printf("%s require list but got ", fun);
+                            print(arg); break; }
+
+        case ARG_LEN0_ERR: {printf("%s require 0 arg but got %d", fun, length(arg));
+                            break; }
+
+        case ARG_LEN1_ERR: {printf("%s require 1 arg but got %d", fun, length(arg));
+                            break; }
+
+        case ARG_LEN2_ERR: {printf("%s require 2 args but got %d", fun, length(arg));
+                            break; }
+
+        case ARG_LEN3_ERR: {printf("%s require 3 args but got %d", fun, length(arg));
+                            break; }
+
+        case MALFORM_ERR:  {printf("%s got malformed args " ,fun);
+                            print(arg); break; }
+    }
+    printf("\n");
+    longjmp(buf,1);
+}
+
+void initsubr() {
+
 }
