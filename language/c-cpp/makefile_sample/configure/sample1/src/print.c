2e262881803e18d23b724dc1a801258e03fff0a3
pogin503 <pogin503@gmail.com>
2023-12-24 18:48:42 +0900

    study: add Autotools sample

---
+++ language/c-cpp/makefile_sample/configure/sample1/src/print.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void print(char *msg)
+{
+  printf("say: %s\n", msg);
+}
---
+++ language/c-cpp/makefile_sample/configure/sample1/src/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "print.h"
+
+int main()
+{
+  print("hello configure!");
+  return 0;
+}
