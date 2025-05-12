fee5ae41188d6b167fd513289f18ddd74add9a28
pogin503 <pogin503@gmail.com>
2025-05-13 03:11:18 +0900

    study: add C# code

---
+++ language/csharp/Usage/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿helloWorld();
+
+outputString();
+
+static void helloWorld()
+{
+    Console.WriteLine("Hello, World!");
+
+    var username = Console.ReadLine();
+    DateTime now = DateTime.Now;
+    Console.WriteLine($"\nHello {username}!");
+}
+
+static void outputString()
+{
+    // someWords is a string array.
+    string[] someWords = {
+        "the",
+        "quick",
+        "brown",
+        "fox",
+        "jumps"
+    };
+
+    string[] moreWords = {
+        "over",
+        "the",
+        "lazy",
+        "dog"
+    };
+
+
+    // Alphabetically sort the words.
+    IEnumerable<string> query = from word in someWords
+                                orderby word
+                                select word;
+
+    foreach (string str in query)
+    {
+        Console.WriteLine(str);
+    }
+}
+
